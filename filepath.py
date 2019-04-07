3693ca96f045b2aa39d6c965b7cead6f24a07108
dkhanna511 <khannaaiig@gmail.com>
2019-04-07 22:49:45 +0530

    added input data

--- filepath.py
+++ filepath.py
@@ -16,8 +16,8 @@ print("\n\n\nimages=\n", images)'''
 #print( " files = \n\n\n\n\n", files)
 #print("\n\n\n\n")
 
-Directory0= "/media/dheeraj/9A26F0CB26F0AA01/WORK/CELESTINI/celestini_task/MiddEval3-GT0-Q/trainingQ/"
-files = os.listdir(Directory0)
+Directory_GT0= "/media/dheeraj/9A26F0CB26F0AA01/WORK/CELESTINI/celestini_task/MiddEval3-GT0-Q/trainingQ/"
+files = os.listdir(Directory_GT0)
 print(files)
 
 '''
@@ -37,7 +37,7 @@ for category in Categories:
 
 images=[]
 for file in files:
-	path=os.path.join(Directory0, file)
+	path=os.path.join(Directory_GT0, file)
 	for img in os.listdir(path):
 		#images.append(img)
 		#print("path= " , "\n\n\n\n", img)
@@ -54,12 +54,12 @@ for file in files:
 		print("img = ", img)
 		cv2.imwrite(GT_Save + "/" +"GT0_" + str(file) + str(img)  , img_array)		
 
-Directory1= "/media/dheeraj/9A26F0CB26F0AA01/WORK/CELESTINI/celestini_task/MiddEval3-GT1-Q/trainingQ/"
-files = os.listdir(Directory1)
+Directory_GT1= "/media/dheeraj/9A26F0CB26F0AA01/WORK/CELESTINI/celestini_task/MiddEval3-GT1-Q/trainingQ/"
+files = os.listdir(Directory_GT1)
 print(files)
 
 for file in files:
-	path=os.path.join(Directory1, file)
+	path=os.path.join(Directory_GT1, file)
 	for img in os.listdir(path):
 		#images.append(img)
 		#print("path= " , "\n\n\n\n", img)
@@ -75,5 +75,31 @@ for file in files:
 		#print(file)
 		print("img = ", img)
 		cv2.imwrite(GT_Save + "/" +  "GT1_"+ str(file) + str(img)  , img_array)		
+
+
+Directory_input= "/media/dheeraj/9A26F0CB26F0AA01/WORK/CELESTINI/celestini_task/MiddEval3-data-Q/trainingQ/"
+files = os.listdir(Directory_input)
+print(files)
+
+for file in files:
+	path=os.path.join(Directory_input, file)
+	for img in os.listdir(path):
+		#images.append(img)
+		#print("path= " , "\n\n\n\n", img)
+		print("path = " , path )
+		image=img.split('.')
+		print(image)
+		#if (img[1]=='png')):
+
+		if (image[1] != 'png'):
+			continue
+
+		img_array=cv2.imread(os.path.join(path, img))
+		#print(file)
+		print("img = ", img)
+		cv2.imwrite(Input_Save + "/" + str(file) + str(img)  , img_array)		
+
+
+
 
 	
